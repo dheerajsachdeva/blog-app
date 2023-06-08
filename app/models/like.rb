e039cb4ed0b15f01834f17ce8a90331181fae58b
Dheeraj Sachdeva <dheerajarya@gmail.com>
2023-06-08 19:23:33 +0530

    Update Linters

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,6 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :post
 
-
   def update_likes_counter
     post.increment!(:likes_counter)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   has_many :comments, class_name: 'Comment', foreign_key: :author_id
   has_many :likes, class_name: 'Like', foreign_key: :author_id
 
-
   def three_recent_posts
     posts.order(created_at: :desc).limit(3)
   end
