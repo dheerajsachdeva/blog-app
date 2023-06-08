5e97066f061da02297a28a851cbf37bf9ce2e262
Dheeraj Sachdeva <dheerajarya@gmail.com>
2023-06-08 18:28:44 +0530

    Update files for processing data

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_many :posts, class_name: 'Post', foreign_key: :author_id
+  has_many :comments, class_name: 'Comment', foreign_key: :author_id
+  has_many :likes, class_name: 'Like', foreign_key: :author_id
+end
+
+def self(_three_recent_posts)
+  post.order(created_at: :desc).limit(3)
 end
