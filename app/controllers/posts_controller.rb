ccfc3a2277c753b1735eb62bda26cca5befef241
Dheeraj Sachdeva <dheerajarya@gmail.com>
2023-06-16 18:46:38 +0530

    Update files for Forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,27 +8,26 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
-  
+
   def new
     @post = Post.new
   end
 
-  def create 
-@post = Post.new(post_params)
-@post.comments_counter = 0
-@post.likes_counter = 0
-@post.author_id = current_user.id
-if @post.save
-redirect_to posts_path, notice: "Post Created Successfully"
-else 
-  render :new
-end
+  def create
+    @post = Post.new(post_params)
+    @post.comments_counter = 0
+    @post.likes_counter = 0
+    @post.author_id = current_user.id
+    if @post.save
+      redirect_to posts_path, notice: 'Post Created Successfully'
+    else
+      render :new
+    end
   end
 
   private
 
-def post_params
-  params.require(:post).permit(:title, :text)
-end
-  
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get "/users", to: "users#index"
   get "/users/:id", to: "users#show", as: 'user'
   get "/users/:id/posts/:id/comments/new", to: "comments#new", as: 'new_comment'
-  post "/users/:id/posts/:id/comments", to: "comments#create"
+  post "/users/:id/posts/:id/comments", to: "comments#create", as: 'comments'
+  post "/users/:id/posts/:id", to: "likes#create", as: 'likes'
 end
 
